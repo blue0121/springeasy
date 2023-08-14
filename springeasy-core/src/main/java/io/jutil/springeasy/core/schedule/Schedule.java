c1f5b2e32a35529497ef33c471979b9d9097ea6e
Jin Zheng <89609719@qq.com>
2023-08-14 16:38:53 +0800

    添加Schedule

--- springeasy-core/src/main/java/io/jutil/springeasy/core/schedule/Schedule.java
+++ springeasy-core/src/main/java/io/jutil/springeasy/core/schedule/Schedule.java
@@ -1,12 +1,21 @@
 package io.jutil.springeasy.core.schedule;
 
+import io.jutil.springeasy.internal.core.schedule.CronSchedule;
+
+import java.util.concurrent.ExecutorService;
+
 /**
  * @author Jin Zheng
  * @since 2023-08-13
  */
 public interface Schedule {
+	static Schedule create(ExecutorService executor) {
+		return new CronSchedule(executor);
+	}
 
 	void add(String id, String cron, Runnable job);
+
+	void add(String id, String cron, Runnable job, ExecutorService executor);
 
 	void remove(String id);
 
---
+++ springeasy-core/src/test/java/io/jutil/springeasy/core/schedule/CronScheduleTest.java
@@ -0,0 +1,56 @@
+package io.jutil.springeasy.core.schedule;
+
+import io.jutil.springeasy.core.util.WaitUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author Jin Zheng
+ * @since 2023-08-14
+ */
+class CronScheduleTest {
+	final ExecutorService executor = Executors.newFixedThreadPool(2,
+			new ExecutorThreadFactory("springeasy"));
+
+	final String jobId = "job1";
+	final String jobCron = "0/1 * * * * *";
+
+	@Test
+	void testSchedule() {
+		var facade = Mockito.mock(TestFacade.class);
+		var schedule = Schedule.create(executor);
+		var job = new TestJob(facade);
+		schedule.add(jobId, jobCron, job);
+		WaitUtil.sleep(1500);
+
+		Mockito.verify(facade, Mockito.atLeast(1)).test();
+
+		schedule.remove(jobId);
+		WaitUtil.sleep(1500);
+		Mockito.verify(facade, Mockito.atMost(2)).test();
+	}
+
+	static interface TestFacade {
+		void test();
+	}
+
+	@Slf4j
+	static class TestJob implements Runnable {
+		TestFacade facade;
+
+		public TestJob(TestFacade facade) {
+			this.facade = facade;
+		}
+
+		@Override
+		public void run() {
+			log.info("Run");
+			facade.test();
+		}
+	}
+
+}
